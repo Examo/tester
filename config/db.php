df830092af62060efd94322176194a9a17a33d25
Admin <admin@examo.ru>
2019-06-29 22:17:32 +0300

    fixed line break

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=examoproject',
+    'dsn' => 'mysql:host=localhost;dbname=examo',
     'username' => 'root',
     'password' => '1234',
     'charset' => 'utf8',
