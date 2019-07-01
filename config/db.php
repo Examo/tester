e33c9ba068100fc5049f49be412b30f108b89277
Yeiazel <vanyatag95@gmail.com>
2019-07-01 23:10:22 +0600

    remove logs

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=examo',
-    'username' => 'root',
-    'password' => '1234',
-    'charset' => 'utf8',
-];
